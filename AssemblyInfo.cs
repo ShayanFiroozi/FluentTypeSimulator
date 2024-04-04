1db4673187484d99385063e792d52d00a29156f9
Shayan Firoozi <shayan.firoozi@gmail.com>
2024-04-04 17:08:53 +0330

    Add project files.

---
+++ AssemblyInfo.cs
@@ -0,0 +1,29 @@
+﻿/*---------------------------------------------------------------------------------------------
+
+                         ► Fluent System Information ◄
+
+
+ → Copyright (c) 2024 Shayan Firoozi , Bandar Abbas , Iran , Under MIT License.
+
+ → Contact : <shayan.firoozi@gmail.com>
+
+ → GitHub repository : https://github.com/ShayanFiroozi/FluentSysInfo.Core
+
+
+---------------------------------------------------------------------------------------------*/
+
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Fluent Type Simulator")]
+[assembly: AssemblyDescription("Simulate typing with many options")]
+[assembly: AssemblyProduct("FluentTypeSimulator")]
+[assembly: AssemblyCopyright("Copyright © 2024 Shayan Firoozi (Under MIT License)")]
+[assembly: AssemblyTrademark("Under MIT License")]
+[assembly: AssemblyCompany("https://github.com/ShayanFiroozi")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.*")]
---
+++ FrmMain.cs
@@ -0,0 +1,12 @@
+using System.Windows.Forms;
+
+namespace FluentTypeSimulator
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
